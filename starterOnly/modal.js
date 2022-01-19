3928ae6332c2b678de5bfe458c47a24133973675
Peters <roselpeters@yahoo.fr>
2022-01-19 11:53:55 +0100

    btn close

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,23 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal event
+const modalClose = document.querySelectorAll(".close-modal");
+modalClose.forEach((e) => e.addEventListener("click", modalFinal));
+
+// Close modal form
+function modalFinal() {
+  modalbg.style.display = "none" ;
 }
 
 
