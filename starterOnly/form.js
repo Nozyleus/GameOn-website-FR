c8e938eca4dcbe977906c1faad3cc0222deb07a4
Peters <roselpeters@yahoo.fr>
2022-02-09 20:03:36 +0100

    finalisation

---
+++ starterOnly/form.js
@@ -0,0 +1,139 @@
+
+
+//verifier la saisi du nom validation first name************************
+
+form.first.addEventListener('change', function(){
+  validFirst(this);
+});
+//variable qui utiliser lors de la validation du formulaire
+let validPrenom = false;
+const validFirst = function(inputFirst){
+  let firstRegExp = new RegExp(
+  '^[a-z]{2,15}$',
+  );
+  // variable qui servira a verifier si le regex est bien respecter
+  let testFirst = firstRegExp.test(inputFirst.value)
+  //variable pour signaler que le champ doit etre rempli
+  let small = document.querySelector('.small1');
+  if(testFirst) {
+      validPrenom = true;
+      small.innerHTML = '';
+      first.style.borderColor = 'white';
+  } else {
+    // insertion du style  si erreur lors de la saisi
+      first.style.borderColor = 'red';
+      small.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
+  }
+
+};
+
+//**verifier la saisi de la validation last name*************************
+form.last.addEventListener('change', function(){
+  validLast(this);
+});
+let validNom = false;
+const validLast = function(inputLast){
+  let lastRegExp = new RegExp(
+    '^[a-z]{2,15}$',
+  );
+  // variable qui servira a verifier si le regex est bien respecter
+  let testLast = lastRegExp.test(inputLast.value)
+  let small = document.querySelector('.small2');
+  if(testLast) {
+      validNom = true;
+    small.innerHTML = '';
+    last.style.borderColor = 'white';
+  } else {
+    // insertion du style  si erreur lors de la saisi
+    last.style.borderColor = 'red';
+    small.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du nom';
+  }
+};
+
+
+//*****************verifiez la bonne saisi de l'emailvalidation Email******************************
+form.email.addEventListener('change', function(){
+  validEmail(this);
+});
+let validMail = false;
+const validEmail = function(inputEmail){
+  let emailRegExp = new RegExp(
+    '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$',
+  );
+  let testEmail = emailRegExp.test(inputEmail.value)
+  let small = document.querySelector('.small3');
+  if(testEmail) {
+      validMail = true;
+    small.innerHTML = '';
+    email.style.borderColor = 'white';
+  } else {
+    email.style.borderColor = 'red';
+    small.innerHTML = 'Veuillez saisir un email correct';
+  }
+};
+
+//***********Fonction pour valider la Date de naissance***************
+form.birthdate.addEventListener('change', function(){
+  validBirdth(this)
+});
+let validAge = false;
+const validBirdth = function(inputBirdth){
+  let birdthRegExp = new RegExp ( 
+    '[0-9]{4}-[0-9]{2}-[0-9]{2}'
+  );
+  let testBirdth = birdthRegExp.test(inputBirdth.value)
+  let small = document.querySelector('.small4');
+  if(testBirdth) {
+      validAge = true;
+    small.innerHTML = '';
+    birthdate.style.borderColor = 'white';
+  } else {
+    birthdate.style.borderColor = 'red';
+    small.innerHTML = 'Vous devez entrer votre date de naissance';
+  }
+
+};
+
+//********pour vérifier la saisi du Number tournament*************************
+form.quantity.addEventListener('change', function(){
+  validQuantity(this);
+});
+let validQuantite = false;
+const validQuantity = function(inputQuantity){
+  let quantityRegExp = new RegExp(
+    '[0-9]'
+  );
+  let testQuantity = quantityRegExp.test(inputQuantity.value);
+  let small = document.querySelector('.small5');
+  
+  if(testQuantity) {
+      validQuantite = true;
+    small.innerHTML = '';
+    quantity.style.borderColor ='white';
+  } else {
+    quantity.style.borderColor = 'red';
+    small.innerHTML = 'Veuillez saisir un nombre';
+  }
+};
+
+
+//VALIDATION MODAL, verifier si chaque input a été rempli correctement avant validation
+document.getElementById ("reserve").addEventListener("submit", function(e){
+  e.preventDefault();
+  let small = document.querySelector ('.small6');
+      // Saisi des variables pour verification avant la validation  
+      if  (validPrenom)
+      if  (validNom)
+      if  (validMail)
+      if  (validAge)
+      if  (validQuantite)
+          {
+            //fermeture du formulaire
+            modalnone();
+            //ouverture de la modal de remerciement
+            modalFinal();
+              
+      } else {
+          small.innerHTML = 'Remplissez tout les champs du formulaire';
+          }
+});
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,10 +1,10 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+  x.className += " responsive";
+} else {
+  x.className = "topnav";
+}
 }
 
 // DOM Elements
@@ -17,6 +17,7 @@ const btnThanks = document.querySelectorAll(".btn_thanks");
 const modalClose = document.querySelectorAll(".close-modal");
 //Permet de chercher les données du formulaire
 const form = document.getElementById("reserve");
+
 //*********recupération des Id du formulaire****************** */
 const first = document.getElementById ("first");
 const last = document.getElementById ('last');
@@ -29,19 +30,19 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+modalbg.style.display = "block";
 }
 //*********lancement modal de remerciement********************************* */
 modalSubmit.forEach((e) => e.addEventListener("click", modalFinal));
 function modalFinal () {
-  modalthks.style.display = "block";}
+modalthks.style.display = "block";}
 
 // close modal event
 modalClose.forEach((e) => e.addEventListener("click", modalnone));
 
 // Close modal form
 function modalnone() {
-  modalbg.style.display = "none" ;
+modalbg.style.display = "none" ;
 }
 
 
@@ -49,132 +50,7 @@ function modalnone() {
 // Close modal thanks***************************
 btnThanks.forEach((button) => button.addEventListener("click", modalThanks));
 function modalThanks() {
-  modalthks.style.display ="none";
+modalthks.style.display ="none";
 }
 
 
-//validation first name************************
-
-form.first.addEventListener('change', function(){
-  validFirst(this);
-});
-
-const validFirst = function(inputFirst){
-  let firstRegExp = new RegExp(
-    '^[a-z]{2,15}$',
-    );
-    let testFirst = firstRegExp.test(inputFirst.value)
-    let small = document.querySelector('.small1');
-    if(testFirst) {
-      small.innerHTML = '';
-      first.style.borderColor = 'white'
-    } else {
-      first.style.borderColor = 'red'
-      small.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
-    }
-  
-  };
-
-//********************validation last name*************************
-form.last.addEventListener('change', function(){
-  validLast(this);
-});
-
-const validLast = function(inputLast){
-  let lastRegExp = new RegExp(
-    '^[a-z]{2,15}$',
-  );
-  let testLast = lastRegExp.test(inputLast.value)
-  let small = document.querySelector('.small2');
-  if(testLast) {
-    small.innerHTML = '';
-    last.style.borderColor = 'white'
-  } else {
-    last.style.borderColor = 'red'
-
-    small.innerHTML = 'Veuillez entrer 2 caractères ou plus pour le champ du nom';
-  }
-};
-
-
-//*****************validation Email******************************
-form.email.addEventListener('change', function(){
-  validEmail(this);
-});
-
-const validEmail = function(inputEmail){
-  let emailRegExp = new RegExp(
-    '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$',
-  );
-  let testEmail = emailRegExp.test(inputEmail.value)
-  let small = document.querySelector('.small3');
-  if(testEmail) {
-    small.innerHTML = '';
-    email.style.borderColor = 'white'
-  } else {
-    email.style.borderColor = 'red'
-    small.innerHTML = 'Veuillez saisir un email correct';
-  }
-};
-
-//*******************Date de naissance***************
-form.birthdate.addEventListener('change', function(){
-  validBirdth(this)
-});
-const validBirdth = function(inputBirdth){
-  let birdthRegExp = new RegExp ( 
-    '[0-9]{4}-[0-9]{2}-[0-9]{2}'
-  );
-  let testBirdth = birdthRegExp.test(inputBirdth.value)
-  let small = document.querySelector('.small4');
-  if(testBirdth) {
-    small.innerHTML = '';
-    birthdate.style.borderColor = 'white'
-  } else {
-    birthdate.style.borderColor = 'red'
-    small.innerHTML = 'Vous devez entrer votre date de naissance';
-  }
-
-};
-
-//Number tournament*************************
-form.quantity.addEventListener('change', function(){
-  validQuantity(this);
-});
-const validQuantity = function(inputQuantity){
-  let quantityRegExp = new RegExp(
-    '[0-9]',
-  );
-  let testQuantity = quantityRegExp.test(inputQuantity.value)
-  let small = document.querySelector('.small5');
-  
-  if(testQuantity) {
-    small.innerHTML = '';
-    quantity.style.borderColor ='white'
-  } else {
-    quantity.style.borderColor = 'red'
-    small.innerHTML = 'Veuillez saisir un nombre';
-  }
-};
-
-
-  //****************VALIDATION MODAL******************* */
-document.getElementById ("reserve").addEventListener("submit", function(e){
-  e.preventDefault();
-  
-  let inputs = Array.from(this.getElementsByClassName("text-control"))
-  let small = document.querySelector ('.small6')
-  inputs.forEach(function (control) {
-
-      if (control.value) {
-        
-        modalnone();
-        modalFinal();
-      }
-
-      else {
-       small.innerHTML = 'Remplissez tout les champs du formulaire'
-        
-      }
-    })
-});
